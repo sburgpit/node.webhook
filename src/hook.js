a41be0b53b6b605c37498e54e7a0b852fa799767
Peter Konokhov <peter.konokhov@tempostorm.com>
2023-08-25 22:58:15 +0300

    refactoring

--- src/hook.js
+++ src/hook.js
@@ -4,60 +4,58 @@ const telegram = require('./telegram')
 const runCommand = require('./runCommand')
 const repos = require('../repos.json')
 
-const messagesChain = []
 const messages = {
-  request: '✅ Got a request\n',
-  noBody: '❌ No body\n',
-  noRepo: (repo) => `❌ There is no \`${repo}\` repository in the config\n`,
-  noActions: (branch) => `❌ There is no actions for \`${branch}\` branch\n`,
-  signature: `❌ Invalid signature\n`,
-  commandPassed: (command, dir) => `✅ \`${command}\` passed in \`${dir}\`\n`,
-  commandFailed: (command, dir) => `❌ \`${command}\` failed in \`${dir}\`\n`,
-  error: (error) => `❌ Error:\n\`${error}\`\n`,
-  success: `✅ Completed successfully\n`,
+  request: '✅ Got a request',
+  noBody: '❌ No body',
+  noRepo: (repo) => `❌ There is no \`${repo}\` repository in the config`,
+  noActions: (branch) => `❌ There is no actions for \`${branch}\` branch`,
+  signature: `❌ Invalid signature`,
+  commandPassed: (command, dir) => `✅ \`${command}\` passed in \`${dir}\``,
+  commandFailed: (command, dir) => `❌ \`${command}\` failed in \`${dir}\``,
+  error: (error) => `❌ Error:\n\`${error}\``,
+  success: `✅ Completed successfully`,
 }
 
 const hook = (req) => async (chunk) => {
+  const messagesChain = []
   const { message_id } = await telegram.sendMessage(messages.request)
   messagesChain.push(messages.request)
 
-  const body = JSON.parse(chunk)
-  if (!body) return telegram.editMessage(`${messagesChain.join('\n ')}${messages.noBody}`, message_id)
+  const editAndPushMessage = async (message) => {
+    await telegram.editMessage(`${messagesChain.join('\n')}\n${message}`, message_id)
+    messagesChain.push(message)
+  }
 
-  const repo = repos.find((repo) => repo.ghRepo === body.repository.name)
-  if (!repo)
-    return telegram.editMessage(`${messagesChain.join('\n ')}${messages.noRepo(body.repository.name)}`, message_id)
+  const body = JSON.parse(chunk)
+  if (!body) return editAndPushMessage(messages.noBody)
+
+  const repo = repos.find((r) => r.ghRepo === body.repository.name)
+  if (!repo) return editAndPushMessage(messages.noRepo(body.repository.name))
 
   const branchName = body.ref.split('refs/heads/')[1]
   const action = repo.actions.find((action) => action.branch === branchName)
-  if (!action) return telegram.editMessage(`${messagesChain.join('\n ')}${messages.noActions(branchName)}`, message_id)
+  if (!action) return editAndPushMessage(messages.noActions(branchName))
 
   const signature = `sha1=${crypto.createHmac('sha1', repo.ghSecret).update(chunk).digest('hex')}`
-  if (req.headers['x-hub-signature'] !== signature)
-    return telegram.editMessage(`${messagesChain.join('\n ')}${messages.signature}`, message_id)
+  if (req.headers['x-hub-signature'] !== signature) return editAndPushMessage(messages.signature)
 
   try {
     for (const commandObj of action.commands) {
-      const command = typeof commandObj === 'string' ? commandObj : commandObj.command
-      const dir = typeof commandObj === 'string' ? action.dir : path.join(action.dir, commandObj.subDir)
+      const { command, subDir } = typeof commandObj === 'string' ? { command: commandObj } : commandObj
+      const dir = subDir ? path.join(action.dir, subDir) : action.dir
 
       try {
         await runCommand(command, dir)
-        const message = messages.commandPassed(command, dir)
-        await telegram.editMessage(`${messagesChain.join('\n ')}${message}`, message_id)
-        messagesChain.push(message)
+        await editAndPushMessage(messages.commandPassed(command, dir))
       } catch (e) {
-        const message = messages.commandFailed(command, dir)
-        await telegram.editMessage(`${messagesChain.join('\n ')}${message}`, message_id)
-        messagesChain.push(message)
-
+        await editAndPushMessage(messages.commandFailed(command, dir))
         throw e
       }
     }
 
-    await telegram.editMessage(`${messagesChain.join('\n')}${messages.success}`, message_id)
+    await editAndPushMessage(messages.success)
   } catch (e) {
-    await telegram.editMessage(`${messagesChain.join('\n')}${messages.error(e)}`, message_id)
+    await editAndPushMessage(messages.error(e))
   }
 }
 
